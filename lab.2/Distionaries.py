55f20d8d62a78eb2d87028d5519fd41bfa847efe
satbaevali <a_satbay@kbtu.kz>
2024-01-24 23:08:28 +0600

    c

---
+++ lab.2/Distionaries.py
@@ -0,0 +1,41 @@
+#task 1
+car ={
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+    
+    
+}
+print(car.get("modul"))
+
+#task 2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"]=2020
+
+#rask 3
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["colar"]="red"
+
+#task 4
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+#task 5
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
