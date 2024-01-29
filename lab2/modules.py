6738c4d70fd46179f85c9698ade91be6f9ae4ab0
satbaevali <a_satbay@kbtu.kz>
2024-01-29 16:23:04 +0600

    lab.2

---
+++ lab2/modules.py
@@ -0,0 +1,17 @@
+#task 1
+#class Student(Person):
+
+#task 2
+
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
